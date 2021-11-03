53909812507a066bcf46d7b2f94e9c409e772e8e
MDK7995 <93629983+MDK7995@users.noreply.github.com>
2021-11-02 22:58:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+/* abre e fecha o menu quando clinar no icone: hambuguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll("nav .toggle")
+
+for(const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links= document.querySelectorAll('nav ul li a')
+
+for(const link of links){
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+        
+    })
+}
+
+/* mudar o header da pagina quando der scroll */
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() { 
+    if(window.scrollY >= navHeight){
+        // scroll e maior que a altura do header
+        header.classList.add('scroll')
+            } else {
+                // menor que a altura do header
+                header.classList.remove('scroll')
+            }
+}
+
+
+/* testimonials carousel sloider swiper */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: 'swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+})
+
+/* ScrollReveal:  MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PAGINA*/
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about . image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+     `, 
+     { interval: 100}
+     )
+
+/* BOTÃO VOLTAR PARA O TOPO*/
+
+
+const backToTopButton = document.querySelector('.back-to-top')
+function backtoTop () {
+
+    if (window.scrollY >=560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* MENU ATIVO CONFORME A SEÇÃO VISIVEL NA PAGINA*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+
+    const checkpoint = window.pageY0ffset + (window.innerHeight / 8) * 4
+
+    for( const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionID = section.getAttribute('id')
+
+        const checkpointStart = cheackpoint >= sectionTop
+        const cheackpointEnd = cheackpoint <= sectionTop + sectionHeight
+        
+        if(checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[hreft*=' + sectionID + ']' )
+            .classList.add('active')
+
+        } else {
+            document
+            .querySelector('nav ul li a[hreft*=' + sectionID + ']' )
+            .classList.remove('active')
+        }
+
+    }
+}
+
+
+/* When Scroll */
+window.addEventListener('scroll', function(){
+    changeHeaderWhenScroll()
+    backtoTop()
+    activateMenuAtCurrentSection()
+})
